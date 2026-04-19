79f2f3a4f3f420bbed222b36560a68e5afced0b0
PGHoon <116252897+taehoon-An@users.noreply.github.com>
2026-04-19 19:03:27 +0900

    [FEAT] TopBar, BottomBar UI 초기 구성 완료

--- app/src/main/java/com/example/letssopt/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/letssopt/ui/home/MainActivity.kt
@@ -7,18 +7,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.letssopt.R
 import com.example.letssopt.ui.theme.AsBg
 import com.example.letssopt.ui.theme.AsSecondaryText
+import com.example.letssopt.ui.theme.AsWhite
 import com.example.letssopt.ui.theme.LETSSOPTTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,20 +44,179 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LETSSOPTTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MainContent(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(70.dp)
+                    .background(AsBg)
+                    .statusBarsPadding()
+                    .padding(horizontal = 6.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.End
+            ) {
+                IconButton(onClick = {}) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.btn_camera),
+                        contentDescription = "camera",
+                        tint = AsWhite
+                    )
+                }
+                IconButton(onClick = {}) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.btn_ring),
+                        contentDescription = "ring",
+                        tint = AsWhite
+                    )
+                }
+                IconButton(onClick = {}) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.btn_profile),
+                        contentDescription = "profile",
+                        tint = AsWhite
+                    )
+                }
+            }
+        },
+        bottomBar = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(72.dp)
+                    .background(AsBg)
+                    .navigationBarsPadding()
+                    .padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                IconButton(
+                    onClick = {},
+                    modifier = Modifier.size(width = 48.dp, height = 50.dp)
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally, // 가로 중앙 정렬
+                        verticalArrangement = Arrangement.Center           // 세로 중앙 정렬
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.btmbar_main),
+                            contentDescription = "mainButton",
+                            tint = AsWhite
+                        )
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(
+                            text = "메인",
+                            style = MaterialTheme.typography.labelSmall, // 작은 글씨 스타일
+                            color = AsWhite
+                        )
+                    }
+                }
+                IconButton(
+                    onClick = {},
+                    modifier = Modifier.size(width = 48.dp, height = 50.dp)
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally, // 가로 중앙 정렬
+                        verticalArrangement = Arrangement.Center           // 세로 중앙 정렬
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.btmbar_category),
+                            contentDescription = "categoryButton",
+                            tint = AsWhite
+                        )
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(
+                            text = "개별 구매",
+                            style = MaterialTheme.typography.labelSmall, // 작은 글씨 스타일
+                            color = AsWhite
+                        )
+                    }
+                }
+                IconButton(
+                    onClick = {},
+                    modifier = Modifier.size(width = 48.dp, height = 50.dp)
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally, // 가로 중앙 정렬
+                        verticalArrangement = Arrangement.Center           // 세로 중앙 정렬
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.btmbar_wallet),
+                            contentDescription = "walletButton",
+                            tint = AsWhite
+                        )
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(
+                            text = "웹툰",
+                            style = MaterialTheme.typography.labelSmall, // 작은 글씨 스타일
+                            color = AsWhite
+                        )
+                    }
+                }
+                IconButton(
+                    onClick = {},
+                    modifier = Modifier.size(width = 48.dp, height = 50.dp)
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally, // 가로 중앙 정렬
+                        verticalArrangement = Arrangement.Center           // 세로 중앙 정렬
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.btmbar_search),
+                            contentDescription = "searchButton",
+                            tint = AsWhite
+                        )
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(
+                            text = "찾기",
+                            style = MaterialTheme.typography.labelSmall, // 작은 글씨 스타일
+                            color = AsWhite
+                        )
+                    }
+                }
+                IconButton(
+                    onClick = {},
+                    modifier = Modifier.size(width = 48.dp, height = 50.dp)
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally, // 가로 중앙 정렬
+                        verticalArrangement = Arrangement.Center           // 세로 중앙 정렬
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.btmbar_folder),
+                            contentDescription = "folderButton",
+                            tint = AsWhite
+                        )
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(
+                            text = "보관함",
+                            style = MaterialTheme.typography.labelSmall, // 작은 글씨 스타일
+                            color = AsWhite
+                        )
+                    }
+                }
+            }
+        }
+    ) { innerPadding ->
+        // 2. Scaffold가 제공하는 padding을 MainContent에 전달
+        MainContent(
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
 @Composable
 fun MainContent(modifier: Modifier = Modifier) {
-    val context = LocalContext.current
-
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -59,6 +235,6 @@ fun MainContent(modifier: Modifier = Modifier) {
 @Composable
 private fun MainContentPreview() {
     LETSSOPTTheme {
-        MainContent()
+        MainScreen()
     }
 }
