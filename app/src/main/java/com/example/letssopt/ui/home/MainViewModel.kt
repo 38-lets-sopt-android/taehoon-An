f5e6282e8e33c1472ae013c26b1adea1aa8bef01
PGHoon <116252897+taehoon-An@users.noreply.github.com>
2026-04-29 14:32:24 +0900

    [REF] enum class 파라미터 선언 변경

--- app/src/main/java/com/example/letssopt/ui/home/MainViewModel.kt
+++ app/src/main/java/com/example/letssopt/ui/home/MainViewModel.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class MainViewModel : ViewModel() {
-    enum class SelectBottomItems(tag: String) {
+    enum class SelectBottomItems(val tag: String) {
         MAIN("메인"),
         CATEGORY("개별구매"),
         WALLET("웹툰"),
@@ -38,7 +38,7 @@ class MainViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(MainUiState(SelectBottomItems.MAIN))
     val uiState = _uiState.asStateFlow()
 
-    fun onSelectBottomItem(selectBottomItem: SelectBottomItems?) {
+    fun onSelectBottomItem(selectBottomItem: SelectBottomItems) {
         _uiState.value = _uiState.value.copy(selectBottomItem = selectBottomItem)
     }
 
