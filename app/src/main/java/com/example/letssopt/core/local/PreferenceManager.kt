6fe49996bdf788cdb234f011f88f747ccec824eb
PGHoon <116252897+taehoon-An@users.noreply.github.com>
2026-05-08 16:16:51 +0900

    [REF] 심화과제를 위한 PrefManager Id 변수 추가

--- app/src/main/java/com/example/letssopt/core/local/PreferenceManager.kt
+++ app/src/main/java/com/example/letssopt/core/local/PreferenceManager.kt
@@ -3,21 +3,30 @@ package com.example.letssopt.core.local
 import android.content.Context
 import com.example.letssopt.core.local.model.AccountItem
 
-class PreferenceManager(context : Context) {
+class PreferenceManager(context: Context) {
     private val sharedPreference = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
 
-    fun setIsLoggedIn(login : Boolean) {
+    fun setId(id: Int) {
+        sharedPreference.edit()
+            .putInt(KEY_ID, id)
+            .apply()
+    }
+
+    fun getId(): Int { return sharedPreference.getInt(KEY_ID, -1) }
+
+    fun setIsLoggedIn(login: Boolean) {
         sharedPreference.edit()
             .putBoolean(KEY_IS_LOGGED_IN, login)
             .apply()
     }
 
-    fun getIsLoggedIn() : Boolean {
-        return sharedPreference.getBoolean(KEY_IS_LOGGED_IN, false) ?: false
+    fun getIsLoggedIn(): Boolean {
+        return sharedPreference.getBoolean(KEY_IS_LOGGED_IN, false)
     }
 
     companion object {
         private const val PREF_NAME = "user_data"
+        private const val KEY_ID = "id"
         private const val KEY_IS_LOGGED_IN = "is_logged_in"
     }
 }
